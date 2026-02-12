b252bd74ab28db948d7529511eea81f2a682d926
Lucas Rocha <guitarralcs@gmail.com>
2026-02-11 22:00:09 -0300

    Barges-Readme V2(refactor)

--- ControllerClass/ControllerAPI.py
+++ ControllerClass/ControllerAPI.py
@@ -40,7 +40,7 @@ class ControllerAPI:
 			# Clicar no 'Ver mais' se existir
 			try:
 				self.driver.find_element(By.XPATH, "(//button[@class='seeMoreButton'])[2]").click()
-				time.sleep(3)
+				time.sleep(5)
 			except:
 				pass
 
